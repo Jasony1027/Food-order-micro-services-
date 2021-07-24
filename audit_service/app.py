7edd3666d958d0c0a551caf7e3b681e48535a01b
Jason Yen <48262605+Jasony1027@users.noreply.github.com>
2021-07-24 13:35:20 -0700

    Update app.py

--- audit_service/app.py
+++ audit_service/app.py
@@ -45,8 +45,6 @@ def get_pickup_order(index):
     # Here we reset the offset on start so that we retrieve messages
     # at the beginning of the message queue.
     # To prevent the for loop from blocking, we set the timeout to 100ms.
-    # There is a risk that this loop never stops if
-    # the index is large and messages are constantly being received!
     consumer = topic.get_simple_consumer(reset_offset_on_start=True,
                                          consumer_timeout_ms=100)
     logger.info("Retrieving pickup at index {}".format(index))
@@ -67,11 +65,6 @@ def get_delivery_order(index):
     """ processes received pickup order data"""
     client = KafkaClient(hosts=HOSTNAME)
     topic = client.topics[TOPIC]
-    # Here we reset the offset on start so that we retrieve messages
-    # at the beginning of the message queue.
-    # To prevent the for loop from blocking, we set the timeout to 100ms.
-    # There is a risk that this loop never stops if
-    # the index is large and messages are constantly being received!
     consumer = topic.get_simple_consumer(reset_offset_on_start=True,
                                          consumer_timeout_ms=100)
     logger.info("Retrieving delivery at index {}".format(index))
